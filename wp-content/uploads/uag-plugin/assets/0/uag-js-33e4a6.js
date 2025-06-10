0731c8f49f271035c964c331e5085b4abc783e4d
wwww-microsoft365 <virtualservices621@gmail.com>
2025-06-10 03:25:35 +0000

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-33e4a6.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-785766bf' );
+});
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-4d2dd5a0' );
+	UAGBTabs.anchorTabId( '.uagb-block-4d2dd5a0' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-4d2dd5a0' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-39aed219' );
+	UAGBTabs.anchorTabId( '.uagb-block-39aed219' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-39aed219' );
+}, false );
+window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-f08b6d90' );
+	UAGBTabs.anchorTabId( '.uagb-block-f08b6d90' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-f08b6d90' );
+}, false );
+ });
